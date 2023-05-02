78eaf8c7d0f83880c4d4e64ebf86bdfb9ea4fd81
rgalvaomachado <rgalvaomachado@gmail.com>
2023-05-02 20:30:23 -0300

    fix nome tabela usuario

--- model/Usuario.php
+++ model/Usuario.php
@@ -9,19 +9,19 @@
         public $grupos;
 
         function buscarTodos(){
-            $buscarTodos =  $this->bd->prepare('SELECT id, nome FROM usuarios ORDER BY nome ASC');
+            $buscarTodos =  $this->bd->prepare('SELECT id, nome FROM usuario ORDER BY nome ASC');
             $buscarTodos->execute();
             return $buscarTodos->fetchAll(PDO::FETCH_ASSOC);
         }
 
         function buscarUsuariosGrupo(){
-            $buscarUsuariosGrupo =  $this->bd->prepare("SELECT id,nome FROM usuarios WHERE grupos like '%#".$this->grupos."#%'");
+            $buscarUsuariosGrupo =  $this->bd->prepare("SELECT id,nome FROM usuario WHERE grupos like '%#".$this->grupos."#%'");
             $buscarUsuariosGrupo->execute();
             return $buscarUsuariosGrupo->fetchAll(PDO::FETCH_ASSOC);
         }
 
         function buscar(){
-            $buscar =  $this->bd->prepare('SELECT id, nome, grupos, email, senha FROM usuarios WHERE id = :id ORDER BY nome ASC');
+            $buscar =  $this->bd->prepare('SELECT id, nome, grupos, email, senha FROM usuario WHERE id = :id ORDER BY nome ASC');
             $buscar->execute([
                 ':id' => $this->id,
             ]);
@@ -29,7 +29,7 @@
         }
 
         function criar(){
-            $criar = $this->bd->prepare('INSERT INTO usuarios (nome, grupos, email, senha) VALUES(:nome, :grupos, :email, :senha)');
+            $criar = $this->bd->prepare('INSERT INTO usuario (nome, grupos, email, senha) VALUES(:nome, :grupos, :email, :senha)');
             $criar->execute([
                 ':nome' => $this->nome,
                 ':grupos' => $this->grupos,
@@ -40,7 +40,7 @@
         }
 
         function editar(){
-            $editar = $this->bd->prepare('UPDATE usuarios SET nome = :nome, grupos = :grupos, email = :email, senha = :senha WHERE id = :id');
+            $editar = $this->bd->prepare('UPDATE usuario SET nome = :nome, grupos = :grupos, email = :email, senha = :senha WHERE id = :id');
             $editar->execute([
               ':id'   => $this->id,
               ':nome' => $this->nome,
@@ -52,7 +52,7 @@
         }
 
         function deletar(){
-            $deletar = $this->bd->prepare('DELETE FROM usuarios where id = :id');
+            $deletar = $this->bd->prepare('DELETE FROM usuario where id = :id');
             $deletar->execute([
               ':id' => $this->id,
             ]);
