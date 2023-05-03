e37cd31f3089760981e598a7ded13b54c5bf7d56
rgalvaomachado <rgalvaomachado@gmail.com>
2023-05-03 15:02:35 -0300

    fix login e includes relatorio no menu

--- view/logon/index.js
+++ view/logon/index.js
@@ -1,28 +1,31 @@
-function login(){
-    var email = $("#emailLogin").val();
-    var senha = $("#senhaLogin").val();
-    $.ajax({
-        method: "POST",
-        url: "../controller/Controller.php",
-        data: {
-            metodo: "login",
-            email: email,
-            senha: md5(senha),
-        },
-        complete: function(response) {
-            var response = JSON.parse(response.responseText);
-            if(response.access){
-                window.location.assign("../home/index.php")
-            }else{
-                const alert = document.getElementById("messageAlert");
-                alert.innerHTML = response.message;
-                setTimeout(function(){
-                    alert.innerHTML = "";
-                }, 2000);
+$(document).ready(function() {
+    $('#login').submit(function(e) {
+        e.preventDefault();
+        var email = $("#emailLogin").val();
+        var senha = $("#senhaLogin").val();
+        $.ajax({
+            method: "POST",
+            url: "../controller/Controller.php",
+            data: {
+                metodo: "login",
+                email: email,
+                senha: md5(senha),
+            },
+            complete: function(response) {
+                var response = JSON.parse(response.responseText);
+                if(response.access){
+                    window.location.assign("../home/index.php")
+                }else{
+                    const alert = document.getElementById("messageAlert");
+                    alert.innerHTML = response.message;
+                    setTimeout(function(){
+                        alert.innerHTML = "";
+                    }, 2000);
+                }
             }
-        }
-    });
-}
+        });
+    })
+});
 
 function logout(){
     $.ajax({
--- view/logon/login.php
+++ view/logon/login.php
@@ -4,20 +4,14 @@
     <link href="styles.css" rel="stylesheet">
     <script src="index.js"></script>
 </head>
-<div id="login">
-    <div class='form'>
-        <label class="title labelLogin">Login</label>
-    </div>
-    <div class='form'>
-        <input class='input' placeholder="Email" id="emailLogin" name="emailLogin" type="email">
-    </div>
-    <div class='form'>
-        <input class='input' placeholder="Senha" id="senhaLogin" name="senhaLogin" type="password">
-    </div>
-    <div class='form'>
-        <input class='button' type="button" onclick="login()" value="Entrar">
-    </div>
-    <div class='form'>
-        <label class="message_alert" id="messageAlert"></label>
-    </div>
-</div>
+<form id="login">
+    <label class="message_alert" id="messageAlert"></label>
+    </br>
+    <label class="title labelLogin">Login</label>
+    </br>
+    <input class='input' placeholder="Email" id="emailLogin" name="emailLogin" type="email" required>
+    </br>
+    <input class='input' placeholder="Senha" id="senhaLogin" name="senhaLogin" type="password" required>
+    </br>
+    <input class='button' type="submit" value="Entrar">
+</form>
