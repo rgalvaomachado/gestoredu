9b6c7c5de638d73a878e50dfe30d449899afed2e
rgalvaomachado <rgalvaomachado@gmail.com>
2023-06-15 13:30:30 -0300

    implemencao parcial da tela de login

--- view/logon/login.php
+++ view/logon/login.php
@@ -4,14 +4,31 @@
     <link href="styles.css" rel="stylesheet">
     <script src="index.js"></script>
 </head>
-<form id="login">
-    <label class="message_alert" id="messageAlert"></label>
-    </br>
-    <label class="title labelLogin">Login</label>
-    </br>
-    <input class='input' placeholder="Email" id="emailLogin" name="emailLogin" type="email" required>
-    </br>
-    <input class='input' placeholder="Senha" id="senhaLogin" name="senhaLogin" type="password" required>
-    </br>
-    <input class='button' type="submit" value="Entrar">
-</form>
+<div class="grid">
+    <div class="grid-item">
+        <div id="esquerda">
+            <img id="logo-gedu" src='../public/img/logo_colorido2.png'>
+            </br></br></br></br>
+            <label class="cinza" id="nao_conta">Ainda não tem uma conta?</label>
+            </br></br>
+            <button class="azul" id="contato">Entre em contato!</button>
+        </div>
+    </div>
+    <div class="grid-item">
+        <div id="direita">
+            <form id="login">
+                <label class="message_alert" id="messageAlert"></label>
+                </br>
+                <label class="title azul">Faça seu Login</label>
+                </br></br></br></br>
+                <input class='input' placeholder="Seu e-mail" id="emailLogin" name="emailLogin" type="email" required>
+                </br>
+                <input class='input' placeholder="Senha" id="senhaLogin" name="senhaLogin" type="password" required>
+                </br></br></br>
+                <input class='button fundo-azul' type="submit" value="Entrar">
+                </br></br>
+                <a class="azul">Esqueci minha senha</a>
+            </form>
+        </div>
+    </div>
+</div>
