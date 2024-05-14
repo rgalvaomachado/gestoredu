aff8594bd6e51a023d66f7463120c9ee5c21baa4
rgalvaomachado <rgalvaomachado@gmail.com>
2024-05-13 21:37:20 -0300

    fix head

--- public/head.php
+++ public/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Gestoredu Hubis</title>
+    <title>Gestoredu</title>
     <link rel="icon" href="/public/img/hubis-icon.png">
     <script src="https://kit.fontawesome.com/7ca1b2bc88.js" crossorigin="anonymous"></script>
     <link href="/public/css/global.css" rel="stylesheet">
