c86ea3d7aea1191d402f005591790676a9f11610
romulo.machado <rgalvaomachado@gmail.com>
2025-07-09 17:00:58 -0300

    fix env

--- env_exemple.php
+++ env_exemple.php
@@ -2,10 +2,10 @@
     $_ENV['BASE_URL'] = 'http://localhost/';
 
     $_ENV['DATABASE_USERNAME'] = 'root';
-    $_ENV['DATABASE_PASSWORD'] = 'root';
-    $_ENV['DATABASE_HOST'] = '127.0.0.1';
+    $_ENV['DATABASE_PASSWORD'] = 'root123';
+    $_ENV['DATABASE_HOST'] = 'db_gestoredu';
     $_ENV['DATABASE_PORT'] = '3306';
-    $_ENV['DATABASE_NAME'] = 'gestoredu';
+    $_ENV['DATABASE_NAME'] = 'gestoredu_bga';
 
     $_ENV['TIMEZONE'] = 'America/Sao_Paulo';
 
